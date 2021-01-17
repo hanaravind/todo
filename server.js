3ce5ce057d604a973d7064f2fc1c2af6443134f7
Aravind <aravind@cloudinfra.in>
2021-01-17 16:36:38 +0530

    added servr to heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/todo'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/spacex/index.html'));
+res.sendFile(path.join(__dirname+'/dist/todo/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
